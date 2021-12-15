b93d46f058fb6a1c446352fa0bc34b6f1a2482fe
Oliver Berg <oliver_berg@hotmail.dk>
2021-12-15 10:34:06 +0100

    oliver js
    Der er tilføjet js til Olivers forklarings video

---
+++ oliver.js
@@ -0,0 +1,27 @@
+/* JavaScript Identifiers */
+//var fodboldKlubben = "OB";
+//document.getElementById ("fodbold").innerHTML = fodboldKlubben;
+
+/* Arrays */
+//let fodboldKlub1 = "OKS";
+//let fodboldKlub2 = "FCK";
+//let fodboldKlub3 = "BIF";
+
+//const fodboldKlubber = ["OKS", "FCK", "BIF"];
+
+/* Loops */
+//for (let i = 0; i < 5; i++) {
+//text += "the number is" + i + "<br>";
+//}
+
+/* if else */
+//if (time <10) {
+  //greetings = "god morgen";
+//} else if (time < 20) {
+  //greetings = "god dag";
+//} else {
+  //greetings = "god aften"
+//}
+
+/* objects */
+//const OB = {by:"Odense", farver:"blå og hvid", placering: "9"};
